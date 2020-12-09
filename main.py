5473ceec663f14bd59e45e74b37573f3e5bb1937
Kenia Sandoval <ksandoval-lopez21@basecampcodingacademy.org>
2020-12-09 11:31:39 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from time import strftime
+
+top= tk.Tk()
+top.title('Clock')
+top.resizable(0,0)
+current_time=strftime("%I:%M:%S %p")
+clock_label=tk.Label(top, font=('calibri', 40, 'bold'), bg="white", fg="pink", text=current_time)
+clock_label.grid(row=0, column=0)
+top.mainloop() 
+
+
+def display_time():
+    current_time=strftime("%M:%S:%p")
+    clock_label.config(text=current_time)
+    top.after(1000, display_time)
+
