d2627b44bd32b58758f37a7ce27a2ad5e6e55fb2
Kenia Sandoval <ksandoval-lopez21@basecampcodingacademy.org>
2020-12-09 14:23:52 -0600

    fixed

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-import tkinter as tk
+import tkinter
+from tkinter import *
+from tkinter.ttk import *
 from time import strftime
-
-top= tk.Tk()
-top.title('Clock')
-top.resizable(0,0)
-current_time=strftime("%I:%M:%S %p")
-clock_label=tk.Label(top, font=('calibri', 40, 'bold'), bg="white", fg="pink", text=current_time)
-clock_label.grid(row=0, column=0)
-top.mainloop() 
-
-
-def display_time():
-    current_time=strftime("%M:%S:%p")
-    clock_label.config(text=current_time)
-    clock_label.after(200, display_time)
+top = tkinter.Tk()
+p1 = PhotoImage(file="download.png")
+top.iconphoto(False, p1)
+top.title("GUI Clock")
+top.resizable(0, 0)
+def time():
+    string = strftime("%I:%M:%S %p")
+    clockTime.config(text=string)
+    clockTime.after(1000, time)
+clockTime = tkinter.Label(
+    top, font=("calibri", 48, "bold"), background="black", foreground="white"
+)
+clockTime.pack(anchor="center")
+time()
+top.mainloop()
