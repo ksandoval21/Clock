b17e549250a0816db61dc2437a9701607d590e84
Kenia Sandoval <ksandoval-lopez21@basecampcodingacademy.org>
2020-12-09 15:14:07 -0600

    complete

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from time import strftime
 top = tkinter.Tk()
 p1 = PhotoImage(file="download.png")
 top.iconphoto(False, p1)
-top.title("GUI Clock")
-top.resizable(0, 0)
+top.title("Clock")
+top.resizable(0,0)
 def time():
     string = strftime("%I:%M:%S %p")
     clockTime.config(text=string)
     clockTime.after(1000, time)
 clockTime = tkinter.Label(
-    top, font=("calibri", 48, "bold"), background="black", foreground="white"
+    top, font=("calibri", 48, "bold"), background="white", foreground="pink"
 )
 clockTime.pack(anchor="center")
 time()
